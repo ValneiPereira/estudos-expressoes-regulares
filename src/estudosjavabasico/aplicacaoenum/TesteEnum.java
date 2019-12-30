11f478c473555565ce87ba0fa438d4e68708f74a
ValneiPereira <valnney@yahoo.com.br>
2019-12-30 17:12:50 -0200

    Organizando somente

--- src/estudosjavabasico/aplicacaoenum/TesteEnum.java
+++ src/estudosjavabasico/aplicacaoenum/TesteEnum.java
@@ -7,9 +7,9 @@ public class TesteEnum {
   public static void main(String[] args) {
     DiaSemana[] dias = DiaSemana.values();
 
-    /*for (int i = 0; i < dias.length; i++) {
+    for (int i = 0; i < dias.length; i++) {
       System.out.println(dias[i]);
-    }*/
+    }
     /*for (DiaSemana diaSemana : dias) {
       System.out.println(diaSemana);
     }*/
